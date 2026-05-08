63dd14d53cadcf3e6298be2a325d258dab2d6d47
SatoNozomi-osushi <kurione.8.30@gmail.com>
2026-05-08 19:47:40 +0900

    ブロック破壊の準備 ブロックの座標などをほかのシートからも管理できるように変更

--- Assets/Scripts/MainGame/BlockSpawn.cs
+++ Assets/Scripts/MainGame/BlockSpawn.cs
@@ -1,23 +1,26 @@
+using NUnit.Framework;
+using Unity.Android.Gradle;
 using UnityEngine;
-
-public class BlockSpawn : MonoBehaviour
-{
-    enum Blocks
+   public enum Blocks
     {
         none=0,
         nomalBlock=1,
         deleted=99
     };
-    Blocks[,] Stage;
+
+public class BlockSpawn : MonoBehaviour
+{
+ 
     public GameObject spawnPoint;
     public Vector2 spawnVec;
     float blockSize = 1;
     public GameObject Block;
+    public static BlockInfomation[,] blockInfo;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Stage = new Blocks[6, 6];
+        blockInfo = new BlockInfomation[6, 6];
         spawnVec = spawnPoint.transform.position;
         for (int i = 0; i < 6; i++) 
         { 
@@ -25,10 +28,15 @@ public class BlockSpawn : MonoBehaviour
             {
                 GameObject go = Instantiate(Block);
                 go.transform.position = new Vector2(spawnVec.x + i * blockSize, spawnVec.y - j * blockSize);
+                go.GetComponent<BlockFall>().blockSize = blockSize;
                 go.GetComponent<BlockFall>().myX = i;
                 go.GetComponent<BlockFall>().myY = j;
-                go.GetComponent<BlockFall>().blockSize = blockSize;
-                Stage[i, j] = Blocks.nomalBlock;
+                blockInfo[i,j] = new BlockInfomation();
+                blockInfo[i, j].posX = i;
+                blockInfo[i, j].posY = j;
+                blockInfo[i, j].blocks = Blocks.nomalBlock;
+                blockInfo[i, j].blockObj = go;
+
             }
 
         }
@@ -38,6 +46,21 @@ public class BlockSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 5; i >= 0; i--)
+        {
+            for (int j = 5; j >= 0; j--)
+            {
+                Debug.Log($"{blockInfo[i, j].posX}{blockInfo[i, j].posY}"  );
+            }
+        }
     }
 }
+
+public class BlockInfomation
+{
+    public int posX;
+    public int posY;
+    public Blocks blocks;
+    public GameObject blockObj;
+}
+
