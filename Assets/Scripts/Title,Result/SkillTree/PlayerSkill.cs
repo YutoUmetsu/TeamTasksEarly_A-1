54e9af206ddf84f908605ff76bb157af40da06bf
kokoro <>
2026-05-29 19:45:03 +0900

    bool ついか

---
+++ Assets/Scripts/Title,Result/SkillTree/PlayerSkill.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    //爆弾ダメージ
+    public bool DamageT1;
+    public bool DamageT2;
+    public bool DamageT3;
+    public bool DamageT4;
+    public bool DamageT5;
+    public bool DamageT6;
+    public bool DamageT7;
+
+    //リザルトボーナス
+    public bool ResultT1;
+    public bool ResultT2;
+    public bool ResultT3;
+    public bool ResultT4;
+    public bool ResultT5;
+    public bool ResultT6;
+    public bool ResultT7;
+
+    //コンボボーナス
+    public bool ComboT1;
+    public bool ComboT2;
+    public bool ComboT3;
+    public bool ComboT4;
+    public bool ComboT5;
+    public bool ComboT6;
+    public bool ComboT7;
+
+    //爆弾範囲
+    public bool RangeT1;
+    public bool RangeT2;
+    public bool RangeT3;
+    public bool RangeT4;
+    public bool RangeT5;
+    public bool RangeT6;
+    public bool RangeT7;
+}
---
+++ Assets/Scripts/Title,Result/SkillTree/SkillType.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public enum SkillType
+{
+    // 爆弾ダメージ
+    DamageT1,
+    DamageT2,
+    DamageT3,
+    DamageT4,
+    DamageT5,
+    DamageT6,
+    DamageT7,
+
+    // リザルト
+    ResultT1,
+    ResultT2,
+    ResultT3,
+    ResultT4,
+    ResultT5,
+    ResultT6,
+    ResultT7,
+
+    // コンボ
+    ComboT1,
+    ComboT2,
+    ComboT3,
+    ComboT4,
+    ComboT5,
+    ComboT6,
+    ComboT7,
+
+    // 範囲
+    RangeT1,
+    RangeT2,
+    RangeT3,
+    RangeT4,
+    RangeT5,
+    RangeT6,
+    RangeT7
+}
