0c9d8dcc2c269ca7e101b795ab8a38dcea4bca45
takeru <>
2026-06-06 16:08:14 +0900

    タイトルロゴのアニメーション追加

--- Assets/Scripts/Title,Result/Title/move1.cs
+++ Assets/Scripts/Title,Result/Title/move1.cs
@@ -1,19 +1,31 @@
 using UnityEngine;
 using DG.Tweening;
 
-public class move1 : MonoBehaviour
+public class Move1 : MonoBehaviour
 {
+    public static bool played = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        RectTransform rt = GetComponent<RectTransform>();
+        if (played) 
+        {
+            transform.position = new Vector3(525.0f, 740.0f, 0.0f);
+            return;
+        }
+        played = true;
+        PlayAnimation();
+        //transform.DOMove(new Vector3(0.0f, 0.0f, 0.0f), 1.0f);
+    }
 
+   void PlayAnimation()
+    {
         Sequence seq = DOTween.Sequence();
 
-        seq.Append(rt.DOAnchorPos(new Vector2(-425f, 260f), 0.2f)); // è„Ç…éùÇøè„Ç∞
-        seq.Append(rt.DOAnchorPos(new Vector2(-425f, 210f), 0.2f)); // ñﬂÇ∑
-        seq.Append(rt.DOAnchorPos(new Vector2(-425f, 240f), 0.15f));
-        seq.Append(rt.DOAnchorPos(new Vector2(-425f, 210f), 0.15f));
+        seq.Append(transform.DOMoveX(525.0f, 2.2f));
+        seq.Join(transform.DOMoveY(740.0f, 2.2f).SetEase(Ease.OutBounce));
+        seq.Join(transform.DOMoveZ(0.0f, 2.2f));
 
-        seq.Append(rt.DOAnchorPos(new Vector2(-425f, 210f), 3.3f)); // ç≈èIà⁄ìÆ
+        seq.Play();
+
     }
 }
